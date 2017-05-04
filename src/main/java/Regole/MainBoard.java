4e4b8d0f6354e8bac097b1f72186e7518cdad8bf
miriampeta <miriampeta02@gmail.com>
2017-05-04 17:51:57 +0200

    sono bravissima parte 2

--- src/main/java/Regole/MainBoard.java
+++ src/main/java/Regole/MainBoard.java
@@ -1,7 +1,17 @@
 package Regole;
 
+import java.util.ArrayList;
+
 public class MainBoard {
 	
+	/*private ArrayList<DevelopementCard> territory;
+	private ArrayList<DevelopementCard> character;
+	private ArrayList<DevelopementCard> building;
+	private ArrayList<DevelopementCard> venture;
+	*/
+	private ArrayList<DevelopementCard,DevelopementCard> ciao;
+
+	private ArrayList<FamilyMember> councilPalace;
 	
 
 }
