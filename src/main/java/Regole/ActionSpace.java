c29ae162b20b7d63fb3fe22fa349e1dee36aaee2
miriampeta <miriampeta02@gmail.com>
2017-05-04 22:39:05 +0200

    da finire le tower e vedii commenti messi in giro, spostare anche le classi nel package giusto. FARE UML! E I COLLEGAMENTI NEL UML

---
+++ src/main/java/Regole/ActionSpace.java
@@ -0,0 +1,16 @@
+package Regole;
+
+public class ActionSpace {
+	
+	private Area areaName;
+	private int activationCost;//valore necessario del dado
+	//private effettoo immediato con fantomatica classe cost
+	private FamilyMember fMember;
+	
+	
+	public ActionSpace(Area areaName, int activationCost, //effetto immediato){
+		this.areaName = areaName;
+		this.activationCost = activationCost;
+		//l'altro
+	}
+}
---
+++ src/main/java/Regole/Tower.java
@@ -0,0 +1,37 @@
+package Regole;
+
+import java.util.ArrayList;
+
+public class Tower {
+	
+	private CardType cardType;
+	private ArrayList<DevelopementCard> cardTower;
+	private ActionSpace actSpaceT1;
+	private ActionSpace actSpaceT2;
+	private ActionSpace actSpaceT3;
+	private ActionSpace actSpaceT4;
+	
+	
+	private Tower(CardType cardType, ArrayList<DevelopementCard> cardTower){
+		this.cardType = cardType;
+		this.cardTower = cardTower;
+		
+		switch(cardType){
+		case TERRITORY:
+			actSpaceT1 = new ActionSpace(Area.TOWERS, 7 /*2 legnetti*/);
+			actSpaceT2 = new ActionSpace(Area.TOWERS, 5 /*1 legnetto*/);
+			actSpaceT3 = new ActionSpace(Area.TOWERS, 0);
+			actSpaceT4 = new ActionSpace(Area.TOWERS, 0);
+		case CHARACTER:
+			actSpaceT1 = new ActionSpace(Area.TOWERS, 7 /*2 legnetti*/);
+			actSpaceT2 = new ActionSpace(Area.TOWERS, 5 /*1 legnetto*/);
+			actSpaceT3 = new ActionSpace(Area.TOWERS, 0);
+			actSpaceT1 = new ActionSpace(Area.TOWERS, 0);
+		
+				
+		 
+		}
+		
+		//decidere se le carte le sceglie o no la torre ad ogni turno e non il game , anche caricare da file
+	}
+}
