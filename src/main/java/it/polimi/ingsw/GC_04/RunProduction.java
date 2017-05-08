7d5030e9aa36e4faaecb79bc628856d5673f3d73
AndreaT <andrea.tricarico@mail.polimi.it>
2017-05-08 11:59:25 +0200

    insieme

---
+++ src/main/java/it/polimi/ingsw/GC_04/RunProduction.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.GC_04;
+
+public class RunProduction implements Action {
+	private int extraDice;
+	
+	
+	public boolean make(FamilyMember fMember, ActionSpace aSpace){
+		
+	}
+	
+
+	
+}
+
---
+++ src/main/java/it/polimi/ingsw/GC_04/takeTerritory.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.GC_04;
+
+public class takeTerritory implements Action {
+	private int extraDice;
+	
+	public boolean tTerritory(FamilyMember fMember, ActionSpace aSpace){
+		if(pBoard.requestedMilitaryPoints <= discM.position){
+			pBoard.territory.add(card);
+			//TODO: si prende gli effetti immediati
+			if(pBoard.territory.size() >= 2)
+				pBoard.requestedMilitaryPoints += pBoard.territory.size() + 1;
+		}else{
+			System.out.println("Non puoi prendere la carta");
+			return false;
+		}
+		
+		
+		
+	}
+
+}
