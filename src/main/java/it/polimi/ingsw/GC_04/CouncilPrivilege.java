110bcc729ca28b27ca479c8f84b0dca6714bef47
AndreaT <andrea.tricarico@mail.polimi.it>
2017-05-14 11:33:11 +0200

    creato privilegio del consiglio NB: la casella councilPalace oltre al privilegio da pure una moneta! ma questa cosa va messa nel rispettivo spazio azione

--- src/main/java/it/polimi/ingsw/GC_04/CouncilPrivilege.java
+++ src/main/java/it/polimi/ingsw/GC_04/CouncilPrivilege.java
@@ -1,5 +1,30 @@
 package it.polimi.ingsw.GC_04;
 
-public class CouncilPrivilege {
+public class CouncilPrivilege extends Effect{
+	
+	public CouncilPrivilege(ResourceType rT){
+		
+		switch (rT){
+		case WOOD:    //to take a stone and a wood: the constructor takes WOOD or STONE indifferently
+		case STONE:
+			effect.put(rT, new Woods(1));
+			effect.put(rT, new Stones(1));
+			break;
+		case SERVANT:
+			effect.put(rT, new Servants(2));
+			break;
+		case COIN:
+			effect.put(rT, new Coins(2));
+			break;
+		case MILITARYP:
+			effect.put(rT, new MilitaryPoints(2));
+			break;
+		case FAITHP:
+			effect.put(rT, new FaithPoints(1));
+			break;
+		}
+				
+	}
+	
 	
 }
