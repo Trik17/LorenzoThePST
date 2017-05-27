ebc2d163b8f1d261a4c638c74660a0c1c0c281a5
AndreaT <andrea.tricarico@mail.polimi.it>
2017-05-27 17:30:25 +0200

    RESURCE ADESSO è ASTRATTA!->UML

--- src/main/java/it/polimi/ingsw/GC_04/model/Resource.java
+++ src/main/java/it/polimi/ingsw/GC_04/model/Resource.java
@@ -1,10 +1,33 @@
 package it.polimi.ingsw.GC_04.model;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
-public class Resource {
-	
+@JsonTypeInfo(use = Id.NAME,
+	include = JsonTypeInfo.As.PROPERTY,
+	property = "type")
+@JsonSubTypes({
+	@Type(value = Coins.class),
+	@Type(value = Woods.class),
+	@Type(value = Stones.class),
+	@Type(value = Servants.class),
+	@Type(value = FaithPoints.class),
+	@Type(value = MilitaryPoints.class),
+	@Type(value = VictoryPoints.class),
+})
+
+public abstract class Resource {
+
 	private int quantity;
+	//constructor needed for Json
+	public Resource() {
+		// TODO Auto-generated constructor stub
+	}
 	
-	public Resource(int quantity){
+	public Resource(@JsonProperty("quantity")int quantity){
 		
 		this.quantity = quantity;
 	}
