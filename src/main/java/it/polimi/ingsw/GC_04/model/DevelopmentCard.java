44fd58d0721cee672fd466fb6c21b6104f820d28
AndreaT <andrea.tricarico@mail.polimi.it>
2017-05-27 17:31:13 +0200

    COMMENTATI, MOMENTANEAMENTE GLI EFFETTI, MANCA LA POSSIBILITà DI AVERE COSTI MULTIPLI

--- src/main/java/it/polimi/ingsw/GC_04/model/DevelopmentCard.java
+++ src/main/java/it/polimi/ingsw/GC_04/model/DevelopmentCard.java
@@ -1,32 +1,66 @@
 package it.polimi.ingsw.GC_04.model;
-
 import java.util.*;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+
+@JsonTypeInfo(use = Id.NAME,
+include = JsonTypeInfo.As.PROPERTY,
+property = "type")
+@JsonSubTypes({
+@Type(value = TerritoryCard.class),
+@Type(value = CharacterCard.class),
+@Type(value = BuildingCard.class),
+@Type(value = VentureCard.class),
+})
 
 public abstract class DevelopmentCard extends Card{
 	
 	private int period;
 	private String name;
-	private ArrayList<Resource> cost;
-	private ArrayList<Effect> immediateEffects;
-	private ArrayList<Effect> permanentEffects;
+	private ArrayList<Resource> cost; //possono avere più di un costo!
+//	private ArrayList<Effect> immediateEffects;
+//	private ArrayList<Effect> permanentEffects;
+	public DevelopmentCard(@JsonProperty("period")int period,@JsonProperty("name") String name,@JsonProperty("cost") ArrayList<Resource> cost){
+		this.period=period;
+		this.name=name;
+		this.cost=cost;
+	}
+	//constructor needed for Json
+		public DevelopmentCard() {
+			// TODO Auto-generated constructor stub
+		}
 	
+
+
 	public abstract void takeCard(Player player,ActionSpace aSpace, FamilyMember fMember,int servants);
 	
-	private void immediateEffect(ArrayList<Effect> effects){};
-	private void permanentEffect(ArrayList<Effect> effects){};
+	//private void immediateEffect(ArrayList<Effect> effects){};
+	//private void permanentEffect(ArrayList<Effect> effects){};
 	
 	public ArrayList<Resource> getCost(){
 		return cost;
 	}
-	
-	public ArrayList<Effect> getImmediateE(){
-		return immediateEffects;
+	public int getPeriod(){
+		return this.period;				
+	}
+	public String getName() {
+		return this.name;
+		
 	}
 	
-	public ArrayList<Effect> getPermanentE(){
-		return permanentEffects;
-	}
+
+	
+//	public ArrayList<Effect> getImmediateE(){
+//		return immediateEffects;
+//	}
+	
+//	public ArrayList<Effect> getPermanentE(){
+//		return permanentEffects;
+//	}
 	
 	
 }
