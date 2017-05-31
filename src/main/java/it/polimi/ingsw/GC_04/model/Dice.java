7c18039af0a89444eba3d67adddf90089726377d
miriampeta <miriampeta02@gmail.com>
2017-05-31 14:31:51 +0200

    cambiato Dice

--- src/main/java/it/polimi/ingsw/GC_04/model/Dice.java
+++ src/main/java/it/polimi/ingsw/GC_04/model/Dice.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.GC_04.model;
 import java.util.Random;
 
 public class Dice {
+	private static final int NEUTRALDICEVALUE = 0;
+	private static final int MINDICEVALUE = 1;
+	private static final int MAXDICEVALUE = 6;
 	
 	private static Dice blackDice;
 	private static Dice whiteDice;
@@ -12,10 +15,12 @@ public class Dice {
 	
 	private Dice() {
 		Random rnd = new Random();
-		value = 1 + rnd.nextInt(6);
+		value = MINDICEVALUE + rnd.nextInt(MAXDICEVALUE);
 	}
 	
-	public Dice(int value) { //it is needed to initialize TakeACardEffect
+	public Dice(int value) {//it is needed to initialize TakeACardEffect
+		if (value < NEUTRALDICEVALUE)
+			this.value = NEUTRALDICEVALUE;
 		this.value = value;
 	}
 	
@@ -23,7 +28,7 @@ public class Dice {
 			blackDice = new Dice();
 			whiteDice = new Dice();
 			orangeDice = new Dice();	
-			neutralDice = new Dice();
+			neutralDice = new Dice(NEUTRALDICEVALUE);
 	}
 	
 	
@@ -32,9 +37,9 @@ public class Dice {
 	public static void rollTheDices(){
 		//it associates a random number between 1 and 6 with the attribute value
 		Random rnd = new Random();
-		blackDice.value = 1 + rnd.nextInt(6);
-		whiteDice.value = 1 + rnd.nextInt(6);
-		orangeDice.value = 1 + rnd.nextInt(6);
+		blackDice.value = MINDICEVALUE + rnd.nextInt(MAXDICEVALUE);
+		whiteDice.value = MINDICEVALUE + rnd.nextInt(MAXDICEVALUE);
+		orangeDice.value = MINDICEVALUE + rnd.nextInt(MAXDICEVALUE);
 	}
 	
 	public int getValue(){
