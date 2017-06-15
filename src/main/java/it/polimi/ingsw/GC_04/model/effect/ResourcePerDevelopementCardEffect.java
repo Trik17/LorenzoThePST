8de9c1b6f8b5bf66a2c303e032b61782376040f8
Unknown <luigispiguzza@hotmail.it>
2017-06-16 00:42:39 +0200

    carte character(da ricontrollare)

--- src/main/java/it/polimi/ingsw/GC_04/model/effect/ResourcePerDevelopementCardEffect.java
+++ src/main/java/it/polimi/ingsw/GC_04/model/effect/ResourcePerDevelopementCardEffect.java
@@ -5,18 +5,18 @@ import it.polimi.ingsw.GC_04.model.card.DevelopmentCard;
 import it.polimi.ingsw.GC_04.model.resource.Resource;
 
 public class ResourcePerDevelopementCardEffect extends ResourceEffect {
-	private DevelopmentCard card;
+	private DevelopmentCard cardType;
 	private Resource bonusResource;
 	
-	public ResourcePerDevelopementCardEffect(DevelopmentCard card, Resource resource) {
-		this.card = card;
+	public ResourcePerDevelopementCardEffect(DevelopmentCard cardType, Resource resource) {
+		this.cardType = cardType;
 		this.bonusResource = resource;
 	}
 
 	@Override
 	public void apply(Player player) {
 		int bonus = bonusResource.getQuantity();
-		int myQuantity = player.getCards(card).size();
+		int myQuantity = player.getCards(cardType).size();
 		if (!(myQuantity == 0)) {
 			bonus = bonus*myQuantity-bonus;
 			bonusResource.addQuantity(bonus);
