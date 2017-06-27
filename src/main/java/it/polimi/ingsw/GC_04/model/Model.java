33651469af8c4ae88edd883eb06024dcb825f1fd
AndreaT <andrea.tricarico@mail.polimi.it>
2017-06-28 00:09:58 +0200

    METTIAMOCI I SERIALIZABLE

--- src/main/java/it/polimi/ingsw/GC_04/model/Model.java
+++ src/main/java/it/polimi/ingsw/GC_04/model/Model.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.GC_04.model;
 
+import java.io.Serializable;
+
 import it.polimi.ingsw.GC_04.Observable;
 import it.polimi.ingsw.GC_04.model.action.Action;
 import it.polimi.ingsw.GC_04.model.resource.Resource;
 
-public class Model extends Observable<Action, Resource>{
+public class Model extends Observable<Action, Resource> implements Serializable{
 	
 	private Player[] players;
 	private int currentPeriod;
