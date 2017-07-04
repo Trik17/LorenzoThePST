963e1953bb96f24dc090745cb0db194be8f03e62
AndreaT <andrea.tricarico@mail.polimi.it>
2017-07-05 00:39:33 +0200

    commentata perchè non va

--- src/main/java/it/polimi/ingsw/GC_04/view/ServerSocketView.java
+++ src/main/java/it/polimi/ingsw/GC_04/view/ServerSocketView.java
@@ -1,8 +1,8 @@
-package it.polimi.ingsw.GC_04.view;
-
-import it.polimi.ingsw.GC_04.Observable;
-import it.polimi.ingsw.GC_04.model.resource.Resource;
-
-public class ServerSocketView extends Observable<String,Resource> {
-
-}
+//package it.polimi.ingsw.GC_04.view;
+//
+//import it.polimi.ingsw.GC_04.Observable;
+//import it.polimi.ingsw.GC_04.model.resource.Resource;
+//
+//public class ServerSocketView extends Observable<String,Resource> {
+//
+//}
