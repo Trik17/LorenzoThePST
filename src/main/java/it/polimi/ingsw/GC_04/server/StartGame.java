d71b3d93492e245394d49448f36399ef54010272
miriampeta <miriampeta02@gmail.com>
2017-07-09 11:46:40 +0200

    tolto attributo model dal player

--- src/main/java/it/polimi/ingsw/GC_04/server/StartGame.java
+++ src/main/java/it/polimi/ingsw/GC_04/server/StartGame.java
@@ -42,7 +42,7 @@ public class StartGame implements Runnable {
 	
 	private void start() {
 		try {clients.forEach((username,stub)-> {
-			players[turn]=new Player(username,turn+1,model);
+			players[turn]=new Player(username,turn+1);
 			turn++;
 		});
 		}catch(Exception e){
