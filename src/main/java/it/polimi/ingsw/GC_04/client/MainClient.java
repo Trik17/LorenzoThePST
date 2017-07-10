07d7acd09616e6a0e702050242f7993c5bd2ebf3
AndreaT <andrea.tricarico@mail.polimi.it>
2017-07-10 18:49:56 +0200

    a

--- src/main/java/it/polimi/ingsw/GC_04/client/MainClient.java
+++ src/main/java/it/polimi/ingsw/GC_04/client/MainClient.java
@@ -13,14 +13,17 @@ import it.polimi.ingsw.GC_04.client.view.ClientRMI;
 public class MainClient {
 	public static void main(String[] args) throws AccessException, RemoteException, NotBoundException {
 	
-		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
 		System.out.println("Choose an unsername: ");
 		String username=in.nextLine();
-        
-		new ClientRMI(username);
-        
-        	
+		System.out.println("Choose CLI or GUI ");
+        String view=in.nextLine();        
+    
+        if (view.equalsIgnoreCase("GUI")) {
+        	ClientRMI client=new ClientRMI(username,true);
+        }else{
+        	ClientRMI client=new ClientRMI(username,false);
+		}  
 		
 	}
 	
